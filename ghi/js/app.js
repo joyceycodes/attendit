4248d4f8da26a24e6393efa7d7ac56ebb4fe9032
Joyce Yu <joyceyu96@gmail.here>
2022-10-11 18:48:37 -0700

    created cards and columns for conferences, added dates and locations

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,59 @@
-window.addEventListener('DOMContentLoaded', () => {
+function createCard(name, description, pictureUrl, startDate, endDate, location) {
+    return `
+      <div class="card shadow p-3 mb-5 bg-body rounded">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <h6 class="card-subtitle">${location}</h6>
+          <p class="card-text">${description}</p>
+        </div>
+        <div class="card-footer">${startDate} - ${endDate}
+        </div>
+      </div>
+    `;
+  }
 
 
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = 'http://localhost:8000/api/conferences/';
+
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error("response not ok")
+        }
+        else {
+            
+            const data = await response.json();
+
+            
+            let index = 0;
+            for(let conference of data.conferences){
+                
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    const title = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    
+                    const startDate = new Date(details.conference.starts).toLocaleDateString(); 
+                    const endDate = new Date(details.conference.ends).toLocaleDateString(); 
+
+                    const location = details.conference.location.name;
+                    console.log(details);
+                    const html = createCard(title, description, pictureUrl,startDate, endDate, location);
+                    
+                    const column = document.querySelector(`#col-${index %3}`);
+                    column.innerHTML += html;
+                    index ++;
+                }
+            }
+        }
+    }
+    catch (e) {
+        console.log("error occurred")
+    }
 });
