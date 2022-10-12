d735360786864bc8e85fc950ea6b2ac88c4e87fe
Joyce Yu <joyceyu96@gmail.here>
2022-10-12 16:45:37 -0700

    conference page and form created

---
+++ ghi/js/new-conference.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const locationUrl = 'http://localhost:8000/api/locations/';
+
+    const locationResponse = await fetch(locationUrl);
+    if(locationResponse.ok){
+        // get list of states from API
+        const locationData = await locationResponse.json();
+        console.log(locationData.locations)
+        // fill in states to HTML form
+        const locationTag = document.getElementById('Location');
+
+        for (let location of locationData.locations){
+            const option = document.createElement('option');
+            
+            option.value = location.id;
+            option.innerHTML = location.name;
+           
+            locationTag.append(option);
+        }
+    }
+
+    // submit POST request to create new location
+    const formTag = document.getElementById('create-conference-form');
+
+    formTag.addEventListener('submit', async event => {
+        event.preventDefault();
+            
+            // get form data
+        const formData = new FormData(formTag)
+
+        // turning formData into JSON object
+        const json = JSON.stringify(Object.fromEntries(formData));
+        const conferenceUrl = 'http://localhost:8000/api/conferences/';
+            
+        const fetchConfig = {
+            method: "post",
+            body: json,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+        
+        const response = await fetch(conferenceUrl, fetchConfig);
+                
+        if (response.ok) {
+            formTag.reset();
+            const newConference = await response.json();
+            console.log(newConference)
+        }
+    });
+});
